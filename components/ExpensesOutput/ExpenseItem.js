7bd0b13a091a8117fe4c2873c373d25da4a0af7a
William Watson <williampwatson1991@gmail.com>
2024-01-15 18:43:16 -0500

    feat: manageexpese title dynamically renders adding or editing based on where its clicked from

--- components/ExpensesOutput/ExpenseItem.js
+++ components/ExpensesOutput/ExpenseItem.js
@@ -4,13 +4,11 @@ import { useNavigation } from '@react-navigation/native';
 import { GlobalStyles } from '../../constants/styles';
 import { getFormattedDate } from '../../util/date';
 
-const ExpenseItem = ({ description, amount, date }) => {
+const ExpenseItem = ({ id, description, amount, date }) => {
   const navigation = useNavigation();
   const expensePressHandler = () => {
     navigation.navigate('ManageExpense', {
-      description,
-      amount,
-      date,
+      expenseId: id,
     });
   };
 
