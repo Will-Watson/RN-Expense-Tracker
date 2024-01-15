59a1a7690c86bd29dabf3614249142e7b14820db
William Watson <williampwatson1991@gmail.com>
2024-01-15 18:50:09 -0500

    feat: dummy button component created in UI folder

---
+++ components/UI/Button.js
@@ -0,0 +1,13 @@
+import { Pressable, View, Text, StyleSheet } from 'react-native';
+
+const Button = () => {
+  return <Pressable>
+    <View>
+      <Text></Text>
+    </View>
+  </Pressable>
+};
+
+const styles = StyleSheet.create({});
+
+export default Button;
