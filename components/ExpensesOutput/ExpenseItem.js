de6c5b21398a0b32b069b767f64d1a73d6314753
William Watson <williampwatson1991@gmail.com>
2024-01-15 17:35:47 -0500

    feat: ExpenseItem dummy component created

---
+++ components/ExpensesOutput/ExpenseItem.js
@@ -0,0 +1,11 @@
+import { Pressable, StyleSheet, View } from 'react-native';
+
+const ExpenseItem = () => {
+  return (
+    <Pressable />
+  )
+};
+
+const styles = StyleSheet.create({});
+
+export default ExpenseItem;
