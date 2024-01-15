bdbacd8061a62e6fcb35badd8ff59e5279588ac5
William Watson <williampwatson1991@gmail.com>
2024-01-15 15:59:59 -0500

    feat: dummy navigation structure set up in app.js

--- App.js
+++ App.js
@@ -3,15 +3,31 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import ManageExpense from './screens/ManageExpense';
+import AllExpenses from './screens/AllExpenses';
+import RecentExpenses from './screens/RecentExpenses';
+
 const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
+
+const ExpensesOverview = () => {
+  return (
+    <BottomTabs.Navigator>
+      <BottomTabs.Screen name='Recent Expenses' component={RecentExpenses} />
+      <BottomTabs.Screen name='All Expenses' component={AllExpenses} />
+    </BottomTabs.Navigator>
+  );
+};
 
 export default function App() {
   return (
     <>
       <StatusBar style='auto' />
       <NavigationContainer>
-        <Stack.Navigator></Stack.Navigator>
+        <Stack.Navigator>
+          <Stack.Screen name='ExpensesOverview' component={ExpensesOverview} />
+          <Stack.Screen name='ManageExpense' component={ManageExpense} />
+        </Stack.Navigator>
       </NavigationContainer>
     </>
   );
