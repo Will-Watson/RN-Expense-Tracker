371598185b769ada4b9d39ebf7c8ace2fc94a4b4
William Watson <williampwatson1991@gmail.com>
2024-01-15 16:44:01 -0500

    feat: expenses output component created and placed in a folder of the same name within the components folder

---
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+const ExpensesOutput = ({expenses}) => {
+  return (
+    <View>
+      <Text>Expenses Output</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ExpensesOutput;
