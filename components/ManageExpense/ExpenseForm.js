6a671932e0eac6923087216cfb641f16f20d2c8a
William Watson <williampwatson1991@gmail.com>
2024-02-01 17:45:47 -0500

    feat: expenseform dummy component created for future reusability. placed in a new folder created called manage expenses in the component folder

---
+++ components/ManageExpense/ExpenseForm.js
@@ -0,0 +1,11 @@
+import { TextInput, StyleSheet } from 'react-native';
+
+const ExpenseForm = () => {
+  return (
+    <TextInput />
+  )
+}
+
+const styles = StyleSheet.create({});
+
+export default ExpenseForm;
