aa1c7fd14992e2cc584e9d4384b8b4f2231cd2f4
William Watson <williampwatson1991@gmail.com>
2024-02-01 18:03:17 -0500

    feat: dummy props added to custom input components

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,12 +1,29 @@
-import { TextInput, View, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import Input from './Input';
 
 const ExpenseForm = () => {
+  const amountChangedHandler = () => {};
+
   return (
     <View>
-      <Input label = "Amount" />
-      <Input label = "Date" />
-      <Input label = "Description" />
+      <Input
+        label='Amount'
+        textInputConfig={{
+          keyboardType: 'decimal-pad',
+          onChangeText: amountChangedHandler,
+        }}
+      />
+      <Input
+        label='Date'
+        textInputConfig={{
+          placeHolder: 'YYYY-MM-DD',
+          maxLength: 10,
+          onChangeText: () => {},
+        }}
+      />
+      <Input label='Description' textInputConfig={{
+        onChangeText: () => {},
+      }} />
     </View>
   );
 };
