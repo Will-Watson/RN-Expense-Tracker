1672cb1ee5d86e8d3b3cbd3bd987e2371e3f3eaf
William Watson <williampwatson1991@gmail.com>
2024-02-01 17:28:12 -0500

    feat: edited return statement in recent expenses to remove anything that might be dated in the future as were only focusing on things that have already been purchased

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -11,7 +11,7 @@ const RecentExpenses = () => {
   const recentExpenses = expensesContext.expenses.filter((expense) => {
     const today = new Date();
     const date7DaysAgo = getDateMinusDays(today, 7);
-    return expense.date > date7DaysAgo;
+    return (expense.date > date7DaysAgo) && (expense.date <= today);
   });
   return (
     <ExpensesOutput expenses={recentExpenses} expensesPeriod='Last 7 Days' />
