108f4473e93d9c4f557955b8531509c151bec2b6
William Watson <williampwatson1991@gmail.com>
2024-02-01 22:04:34 -0500

    feat: setExpenses created for tandem use with context and fetching expenses from firebase. original dummy data removed

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -12,6 +12,7 @@ const RecentExpenses = () => {
   useEffect(() => {
     const getExpenses = async () => {
      const expenses = await fetchExpenses();
+      expensesContext.setExpenses(expenses);
     }
     getExpenses();
   }, []);
@@ -19,10 +20,14 @@ const RecentExpenses = () => {
   const recentExpenses = expensesContext.expenses.filter((expense) => {
     const today = new Date();
     const date7DaysAgo = getDateMinusDays(today, 7);
-    return (expense.date > date7DaysAgo) && (expense.date <= today);
+    return expense.date > date7DaysAgo && expense.date <= today;
   });
   return (
-    <ExpensesOutput expenses={recentExpenses} expensesPeriod='Last 7 Days' fallbackText='No expenses registered for the last 7 days.' />
+    <ExpensesOutput
+      expenses={recentExpenses}
+      expensesPeriod='Last 7 Days'
+      fallbackText='No expenses registered for the last 7 days.'
+    />
   );
 };
 
--- store/expenses-context.js
+++ store/expenses-context.js
@@ -1,71 +1,9 @@
 import { createContext, useReducer } from 'react';
 
-const DUMMY_EXPENSES = [
-  {
-    id: 'e1',
-    description: 'A pair of shoes',
-    amount: 59.99,
-    date: new Date('2024-01-15'),
-  },
-  {
-    id: 'e2',
-    description: 'A pair of trousers',
-    amount: 89.29,
-    date: new Date('2024-01-11'),
-  },
-  {
-    id: 'e3',
-    description: 'Some Bananas',
-    amount: 5.99,
-    date: new Date('2024-01-03'),
-  },
-  {
-    id: 'e4',
-    description: 'Liquor',
-    amount: 14.99,
-    date: new Date('2023-12-31'),
-  },
-  {
-    id: 'e5',
-    description: 'A book',
-    amount: 18.59,
-    date: new Date('2023-12-22'),
-  },
-  {
-    id: 'e6',
-    description: 'A pair of shoes',
-    amount: 59.99,
-    date: new Date('2024-01-15'),
-  },
-  {
-    id: 'e7',
-    description: 'A pair of trousers',
-    amount: 89.29,
-    date: new Date('2024-01-11'),
-  },
-  {
-    id: 'e8',
-    description: 'Some Bananas',
-    amount: 5.99,
-    date: new Date('2024-01-03'),
-  },
-  {
-    id: 'e9',
-    description: 'Liquor',
-    amount: 14.99,
-    date: new Date('2023-12-31'),
-  },
-  {
-    id: 'e10',
-    description: 'A book',
-    amount: 18.59,
-    date: new Date('2023-12-22'),
-  },
-];
-
 export const ExpensesContext = createContext({
   expenses: [],
   addExpense: ({ description, amount, date }) => {},
+  setExpenses: (expenses) => {},
   deleteExpense: (id) => {},
   updateExpense: (id, { description, amount, date }) => {},
 });
@@ -75,6 +13,9 @@ const expensesReducer = (state, action) => {
     case 'ADD':
       const id = new Date().toString() + Math.random().toString();
       return [{ ...action.payload, id }, ...state];
+
+    case 'SET':
+      return action.payload;
 
     case 'UPDATE':
       const updatableExpenseIndex = state.findIndex((expense) => {
@@ -94,10 +35,14 @@ const expensesReducer = (state, action) => {
 };
 
 const ExpensesContextProvider = ({ children }) => {
-  const [expensesState, dispatch] = useReducer(expensesReducer, DUMMY_EXPENSES);
+  const [expensesState, dispatch] = useReducer(expensesReducer, []);
 
   const addExpense = (expenseData) => {
     dispatch({ type: 'ADD', payload: expenseData });
+  };
+
+  const setExpenses = (expenses) => {
+    dispatch({ type: 'SET', payload: expenses });
   };
 
   const deleteExpense = (id) => {
@@ -111,6 +56,7 @@ const ExpensesContextProvider = ({ children }) => {
   const value = {
     expenses: expensesState,
     addExpense,
+    setExpenses,
     deleteExpense,
     updateExpense,
   };
