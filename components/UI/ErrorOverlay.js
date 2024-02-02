0de1b170ade86e5712c0bc00766a0d7a7762a92b
William Watson <williampwatson1991@gmail.com>
2024-02-01 23:36:11 -0500

    feat: styling added to error overlay

--- components/UI/ErrorOverlay.js
+++ components/UI/ErrorOverlay.js
@@ -1,16 +1,35 @@
 import { View, Text, StyleSheet } from 'react-native';
 import Button from './Button';
+import { GlobalStyles } from '../constants/styles';
 
-const ErrorOverlay = ({ message }) => {
+const ErrorOverlay = ({ message, onConfirm }) => {
   return (
     <View style={styles.container}>
-      <Text>An error occured!</Text>
-      <Text>{message}</Text>
-      <Button>Okay</Button>
+      <Text style={[styles.text, styles.title]}>An error occured!</Text>
+      <Text style={styles.text}>{message}</Text>
+      <Button onPress={onConfirm}>Okay</Button>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary700,
+  },
+  text: {
+    color: 'white',
+    textAlign: 'center',
+    marginBottom: 8,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
 
 export default ErrorOverlay;
