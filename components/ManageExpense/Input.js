73d1a8d577c016a753d6002f81f8f625c4bafb2b
William Watson <williampwatson1991@gmail.com>
2024-02-01 19:51:33 -0500

    feat: added error styling

--- components/ManageExpense/Input.js
+++ components/ManageExpense/Input.js
@@ -8,9 +8,13 @@ const Input = ({ label, style, textInputConfig, invalid }) => {
     inputStyles.push(styles.inputMultiline);
   }
 
+  if (invalid) {
+    inputStyles.push(styles.invalidInput);
+  }
+
   return (
     <View style={[styles.inputContainer, style]}>
-      <Text style={styles.label}>{label}</Text>
+      <Text style={[styles.label, invalid && styles.invalidLabel]}>{label}</Text>
       <TextInput style={inputStyles} {...textInputConfig} />
     </View>
   );
@@ -37,6 +41,12 @@ const styles = StyleSheet.create({
     minHeight: 100,
     textAlignVertical: 'top',
   },
+  invalidLabel: {
+    color: GlobalStyles.colors.error500,
+  },
+  invalidInput: {
+    backgroundColor: GlobalStyles.colors.error50,
+  }
 });
 
 export default Input;
