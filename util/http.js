f803ca58e60a784c33084eeaae8a64ef01469522
William Watson <williampwatson1991@gmail.com>
2024-02-01 21:35:27 -0500

    feat: fetchExpenses axios function created for fetching expenses. firebase realtime database url placed in a constant for reusability

--- util/http.js
+++ util/http.js
@@ -1,8 +1,14 @@
 import axios from 'axios';
 
+const BACKEND_URL = 'https://rn-expense-tracker-e7f44-default-rtdb.firebaseio.com';
+
 export const storeExpense = (expenseData) => {
   return axios.post(
-    'https://rn-expense-tracker-e7f44-default-rtdb.firebaseio.com/expenses.json',
+    BACKEND_URL + '/expenses.json',
     expenseData
   );
 };
+
+export const fetchExpenses = () => {
+  axios.get(BACKEND_URL + '/expenses.json');
+}
